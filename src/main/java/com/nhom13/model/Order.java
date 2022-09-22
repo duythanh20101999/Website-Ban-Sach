97ee6b814150ce5ef7ea881a38033cca780c0d0b
LeHieu <lehieu130301@gmail.com>
2022-09-22 14:59:45 +0700

    tao order

---
+++ src/main/java/com/nhom13/model/Order.java
@@ -0,0 +1,22 @@
+package com.nhom13.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order extends BaseModel {
+
+}
